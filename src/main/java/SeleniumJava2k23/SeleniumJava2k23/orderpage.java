91ab6a2f3b2dc765f5bbb9a830d564c1fc647225
moreykrishna <moreykrishna@gmail.com>
2023-08-22 11:53:53 +0530

    pr

--- src/main/java/SeleniumJava2k23/SeleniumJava2k23/orderpage.java
+++ src/main/java/SeleniumJava2k23/SeleniumJava2k23/orderpage.java
@@ -1,5 +1,12 @@
 package SeleniumJava2k23.SeleniumJava2k23;
 
-public class orderpage {
+public class orderpage 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("order pagenejjrjr");
+		System.out.println("order pagenejjrjr");
+		
+	}
 
 }
